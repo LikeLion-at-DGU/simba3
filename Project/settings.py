9ddcf2935b2ce66b25d10d1b8038bed9be3a35f2
chaeunho1227 <chaeunho2002@gmail.com>
2023-06-04 17:51:29 +0900

    feat : User model custom

--- Project/settings.py
+++ Project/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+# auth 기본 모델 설정
 AUTH_USER_MODEL = "users.User"
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Project',
+    'accounts',
     'main',
     'users',
 ]
@@ -127,3 +129,19 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'Project','static')]
 STATIC_ROOT = os.path.join(BASE_DIR, ' static')
+
+
+
+#MAIL Setting
+EMAIL_HOST = 'smtp.gmail.com'
+# 메일을 호스트하는 서버
+EMAIL_PORT = '587'
+# gmail과의 통신하는 포트
+EMAIL_HOST_USER = '********@gmail.com'
+# 발신할 이메일
+EMAIL_HOST_PASSWORD = '********'
+# 발신할 메일의 비밀번호
+EMAIL_USE_TLS = True
+# TLS 보안 방법
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# 사이트와 관련한 자동응답을 받을 이메일 주소,'webmaster@localhost'
