2be1553b3a6276184af101c64c79f39485af17e5
chaeunho1227 <chaeunho2002@gmail.com>
2023-06-08 21:48:27 +0900

    Refactor : "회원가입 script 수정" fetch 방식으로 코드 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('logout/', logout, name="logout"),
     path('signup/',signup,name="signup"),
     path('send_email/',send_email,name="send_email"),
+    path('verify_email/',verify_email,name="verify_email"),
+    path('pw_finder/',pw_finder,name="pw_finder"),
+    path('nickname_redundant_check/',nickname_redundant_check,name="nickname_redundant_check"),
 ]
