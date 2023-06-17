14b4a860415906ea9b36f58c185602e9823dcf7a
chaeunho1227 <chaeunho2002@gmail.com>
2023-06-17 18:34:09 +0900

    feat : "mainpage 적용"

--- main/views.py
+++ main/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def mainpage(request):
-    return render(request, 'main/mainpage.html')
+def mainpage_competition(request):
+    return render(request, 'main/mainpage_competition.html')
+
+def mainpage_supporters(request):
+    return render(request, 'main/mainpage_supporters.html')
+
+def mainpage_entrepreneur(request):
+    return render(request, 'main/mainpage_entrepreneur.html')
