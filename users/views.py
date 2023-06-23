8be22a1818e62e676c0243a0e0b98fc68eef16e1
chaeunho1227 <chaeunho2002@gmail.com>
2023-06-23 15:15:55 +0900

    fix : MY Profile 수정 나이 입력이 없을 때 오류수정

--- users/views.py
+++ users/views.py
@@ -5,7 +5,10 @@ import os
 def mypage(request):
     if request.method == 'POST':
         edit_profile = Profile.objects.get(user=request.user)
-        edit_profile.age = request.POST['age']
+        if request.POST['age'] :
+            edit_profile.age = request.POST['age']
+        else:
+            edit_profile.age = None
         edit_profile.gender = request.POST['gender']
         edit_profile.major = request.POST['major']
         edit_profile.contact = request.POST['contact']
