8712600366a9423a7133f754f7da12715d92e203
IAMBH <xz7840@dgu.edu>
2023-06-28 15:33:27 +0900

    마지막

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,10 @@ from django.conf import settings
 
 app_name ="main"
 urlpatterns = [
-    path('', mainpage_competition, name="mainpage_competition"),
+    path('', start, name="start"),
+    path('mainpage_competition', mainpage_competition, name="mainpage_competition"),
     path('mainpage_entrepreneur/', mainpage_entrepreneur, name="mainpage_entrepreneur"),
     path('mainpage_supporters/', mainpage_supporters, name="mainpage_supporters"),
     path('search/<str:f>', search, name="search"),
-    path('start', start, name="start"),
     path('guide/', guide, name="guide"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
